2c7788ef216ed0c14abc97769c0e289e834d3f2e
Steven Velez <stevendvelez@yahoo.com>
2020-03-25 10:27:39 -0400

    Last few updates

--- src/components/Body/AboutUs/AboutUs.jsx
+++ src/components/Body/AboutUs/AboutUs.jsx
@@ -5,6 +5,8 @@ function AboutUs() {
     return (
         <div class="about-us__container">
             <div id="about-us">
+                <h2>This is the about us section</h2>
+                <p>This is the about us content</p>
                 <Carousel />
             </div>    
         </div>    
--- src/components/Body/AboutUs/Carousel.jsx
+++ src/components/Body/AboutUs/Carousel.jsx
@@ -6,13 +6,13 @@ function Carousel() {
             <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
                 <div class="carousel-inner">
                     <div class="carousel-item active">
-                        <img class="d-block w-100" src="../../../public/images/sleepygrape.png" alt="First slide" />
+                        <img class="d-block w-100" src="" alt="First slide" />
                     </div>
                     <div class="carousel-item">
-                        <img class="d-block w-100" src="../../../public/images/sleepygrape.png" alt="Second slide" />
+                        <img class="d-block w-100" src="" alt="Second slide" />
                     </div>
                     <div class="carousel-item">
-                        <img class="d-block w-100" src="../../../public/images/sleepygrape.png" alt="Third slide" />
+                        <img class="d-block w-100" src="" alt="Third slide" />
                     </div>
                 </div>
             </div>
